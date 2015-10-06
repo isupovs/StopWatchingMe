98cf8ad69cdddd7069752ed74ccb41fdeafb8af8
isupovs <isupovs@github.com>
2015-10-06 10:44:04 +0300

    Changed project type to Win app. Added form for chosing window to be shown. Changed alghorithm that checks existance of a connection.

---
+++ StopWatchingMe/MainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace StopWatchingMe
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.processesListBox = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.mainFormBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.mainFormBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // processesListBox
+            // 
+            this.processesListBox.FormattingEnabled = true;
+            this.processesListBox.Location = new System.Drawing.Point(0, 12);
+            this.processesListBox.Name = "processesListBox";
+            this.processesListBox.Size = new System.Drawing.Size(446, 108);
+            this.processesListBox.TabIndex = 0;
+            this.processesListBox.SelectedIndexChanged += new System.EventHandler(this.processesListBox_SelectedIndexChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 152);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Text = "notifyIcon";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.DoubleClick += new System.EventHandler(this.notifyIcon_DoubleClick);
+            // 
+            // mainFormBindingSource
+            // 
+            this.mainFormBindingSource.DataSource = typeof(StopWatchingMe.MainForm);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 341);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.processesListBox);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.mainFormBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox processesListBox;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.BindingSource mainFormBindingSource;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
---
+++ StopWatchingMe/MainForm.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace StopWatchingMe
+{
+    public partial class MainForm : Form
+    {
+        private readonly List<Process> _processes = new List<Process>();
+        private bool _alreadyConnected;
+        private readonly Thread _hidingThread;
+        private bool _isHiding;
+        private IntPtr _selectedWindowHandler = IntPtr.Zero;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            _hidingThread = new Thread(StartHiding);
+            notifyIcon.Icon = Icon;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            var processes = Process.GetProcesses().Where(p => !string.IsNullOrEmpty(p.MainWindowTitle)).ToList();
+            _processes.AddRange(processes);
+            processesListBox.DisplayMember = "MainWindowTitle";
+            processesListBox.ValueMember = "MainWindowHandle";
+            processesListBox.DataSource = _processes;
+        }
+
+        private void StartHiding()
+        {
+            while (_isHiding)
+            {
+                if (IsPortInUse(5900))
+                {
+                    if (!_alreadyConnected)
+                    {
+                        Console.WriteLine("Connected");
+                        if (_selectedWindowHandler != IntPtr.Zero)
+                        {
+                            ShowWindow(_selectedWindowHandler, ShowWindowCommands.ShowMaximized);
+                            SetForegroundWindow(_selectedWindowHandler);
+                        }
+                        _alreadyConnected = true;
+                    }
+                }
+                else
+                {
+                    if (_alreadyConnected)
+                    {
+                        Console.WriteLine("Disconnected");
+                        _alreadyConnected = false;
+                    }
+                }
+                Thread.Sleep(500);
+            }
+        }
+
+        private static bool IsPortInUse(int port)
+        {
+            IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            TcpConnectionInformation[] tcpConnInfoArray = ipGlobalProperties.GetActiveTcpConnections();
+
+            var c = tcpConnInfoArray.FirstOrDefault(r => r.LocalEndPoint.Port == port && r.State == TcpState.Established);
+            if (c != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow")]
+        private static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ShowWindow(IntPtr hWnd, ShowWindowCommands nCmdShow);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!_isHiding)
+            {
+                btnStart.Text = "Stop";
+                _isHiding = true;
+                _hidingThread.Start();
+            }
+            else
+            {
+                btnStart.Text = "Start";
+                _isHiding = false;
+            }
+        }
+
+        private void processesListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _selectedWindowHandler = (IntPtr)processesListBox.SelectedValue;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _isHiding = false;
+            if (_hidingThread.IsAlive)
+            {
+                _hidingThread.Join();
+            }
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon.Visible = true;
+            }
+        }
+
+        private void notifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = false;
+        }
+
+    }
+
+    internal enum ShowWindowCommands
+    {
+        /// <summary>
+        /// Hides the window and activates another window.
+        /// </summary>
+        Hide = 0,
+
+        /// <summary>
+        /// Activates and displays a window. If the window is minimized or
+        /// maximized, the system restores it to its original size and position.
+        /// An application should specify this flag when displaying the window
+        /// for the first time.
+        /// </summary>
+        Normal = 1,
+
+        /// <summary>
+        /// Activates the window and displays it as a minimized window.
+        /// </summary>
+        ShowMinimized = 2,
+
+        /// <summary>
+        /// Maximizes the specified window.
+        /// </summary>
+        Maximize = 3, // is this the right value?
+
+        /// <summary>
+        /// Activates the window and displays it as a maximized window.
+        /// </summary>      
+        ShowMaximized = 3,
+
+        /// <summary>
+        /// Displays a window in its most recent size and position. This value
+        /// is similar to <see cref="Win32.ShowWindowCommand.Normal"/>, except
+        /// the window is not activated.
+        /// </summary>
+        ShowNoActivate = 4,
+
+        /// <summary>
+        /// Activates the window and displays it in its current size and position.
+        /// </summary>
+        Show = 5,
+
+        /// <summary>
+        /// Minimizes the specified window and activates the next top-level
+        /// window in the Z order.
+        /// </summary>
+        Minimize = 6,
+
+        /// <summary>
+        /// Displays the window as a minimized window. This value is similar to
+        /// <see cref="Win32.ShowWindowCommand.ShowMinimized"/>, except the
+        /// window is not activated.
+        /// </summary>
+        ShowMinNoActive = 7,
+
+        /// <summary>
+        /// Displays the window in its current size and position. This value is
+        /// similar to <see cref="Win32.ShowWindowCommand.Show"/>, except the
+        /// window is not activated.
+        /// </summary>
+        ShowNA = 8,
+
+        /// <summary>
+        /// Activates and displays the window. If the window is minimized or
+        /// maximized, the system restores it to its original size and position.
+        /// An application should specify this flag when restoring a minimized window.
+        /// </summary>
+        Restore = 9,
+
+        /// <summary>
+        /// Sets the show state based on the SW_* value specified in the
+        /// STARTUPINFO structure passed to the CreateProcess function by the
+        /// program that started the application.
+        /// </summary>
+        ShowDefault = 10,
+
+        /// <summary>
+        ///  <b>Windows 2000/XP:</b> Minimizes a window, even if the thread
+        /// that owns the window is not responding. This flag should only be
+        /// used when minimizing windows from a different thread.
+        /// </summary>
+        ForceMinimize = 11
+    }
+}
